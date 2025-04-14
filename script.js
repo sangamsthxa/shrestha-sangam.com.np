726e3800418e9edb6252ceb75bfef97beac431ca
sangam <sangam.shrestha1997@gmail.com>
2025-04-14 14:32:21 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll(".fade-in");
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      {
+        threshold: 0.2,
+      }
+    );
+  
+    elements.forEach(el => observer.observe(el));
+  });
+  
